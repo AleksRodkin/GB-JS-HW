51e78d824474dae6b5b886d3f5a2b5ca24a6ff20
AleksRodkin <aleksrodkin95@gmail.com>
2024-04-19 01:00:19 +0300

    Added completed tasks for homework 10

--- Homework10/script.js
+++ Homework10/script.js
@@ -10,3 +10,66 @@
 // 3. Сформировать контент из данных (картинка загловок и параграф).
 // 4. Добавить данный контент в вёрстку.
 // 5. * Добавить стили при необходимости (по желанию).
+
+const divContEl = document.querySelector("div.container");
+
+const spawnJokeButton = document.createElement('button');
+spawnJokeButton.classList.add('spawn');
+spawnJokeButton.innerHTML = "New joke"
+divContEl.appendChild(spawnJokeButton);
+
+spawnJokeButton.addEventListener('click', function (e) {
+  if (document.querySelector('div.joke__container')){
+    document.querySelector('div.joke__container').remove();
+  }
+  fetchJoke();
+});
+
+function fetchJoke() {
+  fetch("https://official-joke-api.appspot.com/random_joke")
+  .then((response) => response.json())
+  .then((parseResult) => {
+    const newDivEl = document.createElement("div");
+    newDivEl.classList.add("joke__container");
+
+    const jokeType = document.createElement('p');
+    jokeType.innerHTML = `Joke's type: ${parseResult.type}`;
+    newDivEl.appendChild(jokeType);
+
+    const separatorLine = document.createElement('div');
+    separatorLine.classList.add('separator');
+    newDivEl.appendChild(separatorLine);
+
+    const jokeSetup = document.createElement('p');
+    jokeSetup.innerHTML = parseResult.setup;
+    newDivEl.appendChild(jokeSetup);
+
+    const punchlineButton = document.createElement('button');
+    punchlineButton.innerHTML = 'Show answer';
+    newDivEl.appendChild(punchlineButton);
+    punchlineButton.addEventListener('click', function (e) {
+      if (jokePunchline.classList.contains('hidden')) {
+        jokePunchline.classList.remove('hidden');
+        jokePunchline.classList.add('visible');
+        punchlineButton.innerHTML = "Hide answer"
+      } else if (jokePunchline.classList.contains('visible')) {
+        jokePunchline.classList.remove('visible');
+        jokePunchline.classList.add('hidden');
+        punchlineButton.innerHTML = "Show answer"
+      }
+    });
+
+    const jokePunchline = document.createElement('p');
+    jokePunchline.classList.add("hidden");
+    jokePunchline.innerHTML = parseResult.punchline;
+    newDivEl.appendChild(jokePunchline);
+
+    newDivEl.appendChild(separatorLine.cloneNode());
+
+    const jokeId = document.createElement('p');
+    jokeId.innerHTML = `Joke's ID: ${parseResult.id}`;
+    newDivEl.appendChild(jokeId);
+
+    divContEl.appendChild(newDivEl);
+  });
+}
